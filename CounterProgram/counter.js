4ab8ffb34362ea13da18b4cf7b92372e245af5b1
YuvanShankar <109721267+yuvanshankar2694@users.noreply.github.com>
2023-10-05 23:42:02 +0530

    Add files via upload
    Counter Program using JavaScript, HTML,CSS

---
+++ CounterProgram/counter.js
@@ -0,0 +1,14 @@
+let count=0;
+
+document.getElementById('decreasebtn').onclick = function () {
+    count-=1;
+    document.getElementById('counterLable').innerHTML=count;
+}
+document.getElementById('resetbtn').onclick = function () {
+  count = 0
+  document.getElementById('counterLable').innerHTML = count
+}
+document.getElementById('increasebtn').onclick = function () {
+  count += 1
+  document.getElementById('counterLable').innerHTML = count
+}
